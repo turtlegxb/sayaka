9c53c5224818a0a173664df65fe0966a4fc2ae46
turtlegxb <turtleforu@gmail.com>
2026-04-04 01:39:15 +0800

    feat: configure app to bind to all network interfaces by setting host to 0.0.0.0

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def post(post_id):
                            content=html_content)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host='0.0.0.0')
