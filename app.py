e0217a58d693f1ced45469474c5992b211b88968
turtlegxb <turtleforu@gmail.com>
2026-04-06 16:00:10 +0800

    fix: update application port from 5001 to 5000

--- app.py
+++ app.py
@@ -124,4 +124,4 @@ def catch_all(req_path):
                            content=html_content)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='0.0.0.0')
